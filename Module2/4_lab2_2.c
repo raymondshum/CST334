4ec5e3fa85d0af8fec132b3fa482c324a133628c
raymondshum <rshum@csumb.edu>
2021-03-15 10:04:23 -0700

    Updated lab 2 headers & command-line validation.

--- Module2/4_lab2_2.c
+++ Module2/4_lab2_2.c
@@ -1,4 +1,12 @@
-/*Sample C program for Lab assignment 1*/
+/*
+* NAME: Raymond Shum
+* DATE: March 16, 2021
+* TITLE: Lab 3 - Part 4 - Revise C Code for Demonstration
+* DESCRIPTION: This program creates a child process, which
+* executes the Linux shell command "ls", The parent process
+* waits for the child to complete before exiting. Intended
+* microsecond delay is passed as a command line argument.
+*/
 
 #include <stdio.h>     /* printf, stderr */
 #include <sys/types.h> /* pid_t */
@@ -11,10 +19,25 @@
 int main(int argc, char *argv[])
 {
     pid_t pid;
-    int i, n = atoi(argv[1]); // n microseconds to input from keyboard for delay
-    printf("\n Before forking.\n");
+    int i, n; // n microseconds to input from keyboard for delay
 
+    //Checks to see if a command-line argument has been passed
+    if(argc != 2)
+    {
+        //Microsecond delay is set to 0 if no argument is passed.
+        printf("Error: No value for milisecond delay has been passed as a command-line argument.");
+        printf(" Defaulting to a value of 0.");
+        n = 0;
+    }
+    else
+    {
+        //Microsecond delay is set to value of argument if one has been passed.
+        n = atoi(argv[1]);
+    }
+
+    printf("\n Before forking.\n");
     pid = fork();
+
     if (pid == -1)
     {
         fprintf(stderr, "can't fork, error %d\n", errno);
--- Module2/run.sh
+++ Module2/run.sh
@@ -1,4 +1,20 @@
+# ############################################################
+# NAME: Raymond Shum                                         #
+# DATE: March 16, 2021                                       #
+# TITLE: Lab 3 - Run script                                  #
+# DESCRIPTION: This script is not part of the assignment. It #
+# compiles and runs both parts of the lab assignment.        #
+# ############################################################
+
+# Cleans old executables, compiles programs and creates run1 output.
+make clean
+make
 touch 3_run1.txt
+
+# Run program 1 with 0, 3000, 500, 5000 microsecond delays. Redirects 
+# stdout to text file.
+echo -e "\n*** RUN 1 - PROGRAM: 1_lab2_1 - 0ms DELAY ***\n" > 3_run1.txt
+./1_lab2_1 0 >> 3_run1.txt
 
 echo -e "\n*** RUN 1 - PROGRAM: 1_lab2_1 - 3000ms DELAY ***\n" > 3_run1.txt
 ./1_lab2_1 3000 >> 3_run1.txt
@@ -7,9 +23,8 @@ echo -e "\n*** RUN 2 - PROGRAM: 1_lab2_1 - 500ms DELAY ***\n" >> 3_run1.txt
 ./1_lab2_1 500 >> 3_run1.txt
 
 echo -e "\n*** RUN 3 - PROGRAM: 1_lab2_1 - 5000ms DELAY ***\n" >> 3_run1.txt
-./1_lab2_1 3000 >> 3_run1.txt
+./1_lab2_1 5000 >> 3_run1.txt
 
-touch 6_run2.txt
-
+# Runs program 2, outputting to console
 echo -e "\n*** Run 1 - Program 4_lab2_2 - 3000ms DELAY ***\n" > 6_run2.txt
-./4_lab2_2 3000 >> 6_run2.txt
+./4_lab2_2 3000
