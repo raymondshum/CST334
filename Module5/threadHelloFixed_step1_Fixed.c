8fe9e5dc8ef792446ed5eaab6dd5b8547044a9a7
raymondshum <rshum@csumb.edu>
2021-04-03 20:12:02 -0700

    Updated Step 1 and Step 1 Fix.

--- Module5/threadHelloFixed_step1_Fixed.c
+++ Module5/threadHelloFixed_step1_Fixed.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 void *go(void *);
@@ -9,15 +9,28 @@ pthread_t threads[NTHREADS];
 int main()
 {
     int i;
+    int *array[NTHREADS];
+
 
     for (i = 0; i < NTHREADS; i++)
-        pthread_create(&threads[i], NULL, go, &i);
+    {
+        int *tempPtr = (int *) malloc(sizeof(int));
+        *tempPtr = i;
+        array[i] = tempPtr;
+
+        pthread_create(&threads[i], NULL, go, array[i]);
+    }
 
     for (i = 0; i < NTHREADS; i++)
     {
         printf("Thread %d returned\n", i);
         pthread_join(threads[i], NULL);
     }
+
+     for (i = 0; i < NTHREADS; i++)
+     {
+         free(array[i]);
+     }
 
     printf("Main thread done.\n");
     return 0;
