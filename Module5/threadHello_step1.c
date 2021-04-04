654934367c82d0e7a50c7b0b2a376b98b7574c0b
raymondshum <rshum@csumb.edu>
2021-04-03 17:45:08 -0700

    Module 5 initial commit.

---
+++ Module5/threadHello_step1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *go(void *);
+#define NTHREADS 10
+pthread_t threads[NTHREADS];
+
+int main()
+{
+    int i;
+    int ptrArray[NTHREADS];
+    int *tempPtr;
+
+    for (i = 0; i < NTHREADS; i++)
+        *tempPtr = (int **) malloc(sizeof(int));
+        *tempPtr = i;
+        ptrArray[i] = *tempPtr;
+        pthread_create(&threads[i], NULL, go, &ptrArray[i]);
+
+    for (i = 0; i < NTHREADS; i++)
+    {
+        printf("Thread %d returned\n", i);
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Main thread done.\n");
+    return 0;
+}
+
+void *go(void *arg)
+{
+    printf("Hello from thread %d with iteration %d\n", (int)pthread_self(), *(int *)arg);
+    return 0;
+}
