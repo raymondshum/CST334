95584fde49d70aa4bc6fc765b8c25bdcafaa91aa
raymondshum <rshum@csumb.edu>
2021-04-03 21:35:17 -0700

    matrix_multiplication.c test build

--- Module5/matrix_multiplication.c
+++ Module5/matrix_multiplication.c
@@ -36,25 +36,51 @@ int main()
         for (int j = 0; j < L; j++)
         {
             matrixB[i][j] = rand() % 10;
-            printf("%.2f ", matrixB[i][j]);
+            printf("%.2f ", matrixB[i][j], i, j);
         }
         printf("\n");
     }
 
-    for(int i = 0; i < N; i++) {
-        int *ptr = malloc(sizeof(int));
-        *ptr = i;
-        pthread_create(&threads[i], NULL, matrixMult, ptr);
+    for (int i = 0; i < N; i++)
+    {
+        int *tempPtr = (int *)malloc(sizeof(int));
+        *tempPtr = i;
+        pthread_create(&threads[i], NULL, matrixMult, tempPtr);
+        
     }
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         printf("Thread %d returned\n", i);
         pthread_join(threads[i], NULL);
+    }
+
+    printf("\nMatrix C:\n");
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < L; j++)
+        {
+            printf("%.2f ", matrixC[i][j], i, j);
+        }
+        printf("\n");
     }
 
     return 0;
 }
 
-void *matrixMult(void *arg){
+void *matrixMult(void *arg)
+{
     printf("Thread %d of iter %d\n", (int)pthread_self(), *(int *)arg);
+    int i = *(int *)arg;
+    for (int j = 0; j < L; j++)
+    {
+        double temp = 0;
+        for (int k = 0; k < M; k++)
+        {
+            temp += matrixA[i][k] * matrixB[k][j];
+                printf("(matrixA: %.2f * matrixB: %.2f) = %.2f ",matrixA[i][k], matrixB[k][j], matrixA[i][k] * matrixB[k][j]);
+        }
+        matrixC[i][j] = temp;
+        printf("Total: %.2f\n",temp);
+    }
 }
