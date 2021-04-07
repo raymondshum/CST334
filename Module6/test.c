7d44c95731cb4031c90a54478730e6ee3a57db7b
raymondshum <rshum@csumb.edu>
2021-04-07 12:18:11 -0700

    test.c working build

--- Module6/test.c
+++ Module6/test.c
@@ -14,31 +14,49 @@ pthread_t conThreads[CON_THREAD];
 sem_t empty, full, mutex;
 
 char letter[ALPHABET];
-char results[ALPHABET];
+char buffer[BUFFER];
+
+static int position = 0;
+static int fill = 0;
+static int use = 0;
 
 void initLetter();
 void initResults();
 void printArray(char arg[]);
 void *producer(void *arg);
 void *consumer(void *arg);
-void put(void *arg);
-char get(void *arg);
+void put(char arg);
+char get();
 
 int main(){
+
+
 
     sem_init(&empty, 0, BUFFER);
     sem_init(&full, 0, 0);
     sem_init(&mutex, 0, 1);
     
     initLetter();
-    initResults();
 
     int i = 0;
-    pthread_create(&proThreads[i], NULL, producer, &i);
-    pthread_create(&proThreads[i], NULL, consumer, &i);
-    pthread_join(proThreads[i], NULL);
-    pthread_join(conThreads[i], NULL);
 
+    for (i = 0; i < PRO_THREAD; i++) {
+        pthread_create(&proThreads[i], NULL, producer, &i);
+    }
+    
+    for (i = 0; i < CON_THREAD; i++) {
+        pthread_create(&conThreads[i], NULL, consumer, &i);
+    }
+
+    for (i = 0; i < PRO_THREAD; i++ ){
+        pthread_join(proThreads[i], NULL);
+    }
+
+    for (i = 0; i < CON_THREAD; i++) {
+        pthread_join(conThreads[i], NULL);
+    }
+
+    printf("Program Done.\n");
     return 0;
 }
 
@@ -56,15 +74,6 @@ void initLetter(){
 
 }
 
-void initResults(){
-    for(int i = 0; i < ALPHABET; i++){
-        results[i] = ' ';
-    }
-
-    printf("Contents of Results Array: ");
-    printArray(results);
-}
-
 void printArray(char arg[]){
     for(int i = 0; i < ALPHABET; i++) {
         printf("%c ", arg[i]);
@@ -74,14 +83,49 @@ void printArray(char arg[]){
 
 void *producer(void *arg){
     
-    printf("Producer: %d | Iteration: %d\n", (int)pthread_self(), *(int*)arg);
+    printf("ProducerT: %d | Iteration: %d\n", (int)pthread_self(), *(int*)arg);
+    do {
+        sem_wait(&empty);
+        sem_wait(&mutex);
+
+        printf("Produce: %c\n", letter[position]);
+        put(letter[position]);
+        position++;
+
+        sem_post(&mutex);
+        sem_post(&full);
+    } while(position < ALPHABET);
 
     return (NULL);
 }
 
 void *consumer(void *arg){
 
-        printf("Consumer: %d | Iteration: %d\n", (int)pthread_self(), *(int*)arg);
+        printf("ConsumerT: %d | Iteration: %d\n", (int)pthread_self(), *(int*)arg);
+        char sentinel = ' ';
+
+        do {
+            
+            sem_wait(&full);
+            sem_wait(&mutex);
+
+            sentinel = get();
+            printf("Consume: %c\n", sentinel);
+
+            sem_post(&mutex);
+            sem_post(&empty);
+        } while(position < ALPHABET);
 
     return (NULL);
+}
+
+void put(char arg) {
+    buffer[fill] = arg;
+    fill = (fill + 1) % BUFFER;
+}
+
+char get() {
+    char tmp = buffer[use];
+    use = (use + 1) % BUFFER;
+    return tmp;
 }
